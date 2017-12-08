993b4dd4f1d26075546ca108f3a966b989d5572a
hengwu <youchuangqiba@163.com>
2017-12-09 00:45:51 +0800

    upd8: npm.

--- test/index.js
+++ test/index.js
@@ -2,4 +2,17 @@ const path = require('path');
 
 const intf = require('../src/index');
 
-console.log(intf.stringify(intf.parse(path.resolve(__dirname, 'combin-title-desc'))));
+let node1 = intf.parse(path.resolve(__dirname, 'combin-title-desc'));
+
+console.log(node1);
+
+console.log(intf.stringify(node1));
+
+let node2 = intf.parse(path.resolve(__dirname, 'layout-fix-flex'));
+
+console.log(node2);
+
+node1.slots[0].nodes.push(node2);
+
+console.log(intf.stringify(node1));
+
