83d51a4fbb9962b3bf7b9a869c602ca3e1e75d44
hengwu <youchuangqiba@163.com>
2017-12-25 18:27:00 +0800

    upd8

--- src/parseSass.js
+++ src/parseSass.js
@@ -2,6 +2,83 @@ const util = require('./util');
 
 const sass = require('node-sass');
 
+const descPresets = {
+    text: {
+        _type: 'text'
+    },
+    color: {
+        _type: 'color'
+    },
+    number: {
+        _type: 'number'
+    },
+    size: {
+        number: {
+            _type: 'number'
+        },
+        unit: {
+            _type: 'select',
+            _options: ['px', '%', 'em', 'rem']
+        }
+    },
+    size_4: {
+        top: {
+            number: {
+                _type: 'number'
+            },
+            unit: {
+                _type: 'select',
+                _options: ['px', '%', 'em', 'rem']
+            }
+        },
+        right: {
+            number: {
+                _type: 'number'
+            },
+            unit: {
+                _type: 'select',
+                _options: ['px', '%', 'em', 'rem']
+            }
+        },
+        bottom: {
+            number: {
+                _type: 'number'
+            },
+            unit: {
+                _type: 'select',
+                _options: ['px', '%', 'em', 'rem']
+            }
+        },
+        left: {
+            number: {
+                _type: 'number'
+            },
+            unit: {
+                _type: 'select',
+                _options: ['px', '%', 'em', 'rem']
+            }
+        }
+    },
+    display: {
+        _type: 'select',
+        _options: ['block', 'inline-block', 'inline']
+    },
+    position: {
+        _type: 'select',
+        _options: ['relative', 'absolute', 'fixed']
+    },
+    float: {
+        _type: 'select',
+        _options: ['left', 'right']
+    },
+    'text-align': {
+        _type: 'select',
+        _options: ['center', 'left', 'right']
+    }
+};
+
+const cloneObj = obj => JSON.parse(JSON.stringify(obj));
+
 const parse = (filePath, { converter } = {}) => {
     let content = util.readFile(filePath);
     if (content == null) return null;
@@ -10,7 +87,7 @@ const parse = (filePath, { converter } = {}) => {
     let varArr = t.matches.map(item => ({
         name: item[1],
         value: item[2],
-        desc: item[3].trim()
+        desc: cloneObj(descPresets[item[3].trim()] || { _type: 'text' })
     }));
     return {
         vars: converter ? converter(varArr) : varArr,
