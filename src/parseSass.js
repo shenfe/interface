3046c716b6c693cb931fee65a3a483085510cf4c
hengwu <youchuangqiba@163.com>
2017-12-25 21:46:53 +0800

    upd8

--- src/parseSass.js
+++ src/parseSass.js
@@ -2,78 +2,99 @@ const util = require('./util');
 
 const sass = require('node-sass');
 
-const descPresets = {
-    text: {
-        _type: 'text'
-    },
-    color: {
-        _type: 'color'
-    },
-    number: {
-        _type: 'number'
-    },
-    size: {
-        number: {
-            _type: 'number'
-        },
-        unit: {
-            _type: 'select',
-            _options: ['px', '%', 'em', 'rem']
-        }
-    },
-    size_4: {
-        top: {
-            number: {
-                _type: 'number'
-            },
-            unit: {
-                _type: 'select',
-                _options: ['px', '%', 'em', 'rem']
+const units = ['px', '%', 'rem', 'em'];
+
+const valueDecoder = (type, v) => {
+    switch (type) {
+        case 'text':
+        case 'color':
+        case 'number':
+            return {
+                _type: type,
+                _value: v
+            };
+        case 'size':
+            if (!v) v = '0px';
+            let number, unit;
+            number = parseFloat(v);
+            if (isNaN(number)) {
+                number = 0;
+                unit = 'px';
+            } else {
+                unit = units.find(u => v.indexOf(u) >= 0) || 'px'
             }
-        },
-        right: {
-            number: {
-                _type: 'number'
-            },
-            unit: {
-                _type: 'select',
-                _options: ['px', '%', 'em', 'rem']
+            return {
+                number: {
+                    _type: 'number',
+                    _value: number
+                },
+                unit: {
+                    _type: 'select',
+                    _value: unit,
+                    _options: units
+                }
+            };
+        case 'size_4':
+            let trbl = v.split(' ');
+            if (trbl.length === 1) {
+                trbl.push(trbl[0]);
+                trbl.push(trbl[0]);
+                trbl.push(trbl[0]);
+            } else if (trbl.length === 2) {
+                trbl.push(trbl[0]);
+                trbl.push(trbl[1]);
+            } else if (trbl.length === 3) {
+                trbl.push(trbl[1]);
             }
-        },
-        bottom: {
-            number: {
-                _type: 'number'
-            },
-            unit: {
+            return {
+                top: valueDecoder('size', trbl[0]),
+                right: valueDecoder('size', trbl[1]),
+                bottom: valueDecoder('size', trbl[2]),
+                left: valueDecoder('size', trbl[3])
+            };
+        case 'display':
+            return {
                 _type: 'select',
-                _options: ['px', '%', 'em', 'rem']
-            }
-        },
-        left: {
-            number: {
-                _type: 'number'
-            },
-            unit: {
+                _value: v,
+                _options: ['block', 'inline-block', 'inline']
+            };
+        case 'position':
+            return {
                 _type: 'select',
-                _options: ['px', '%', 'em', 'rem']
-            }
-        }
-    },
-    display: {
-        _type: 'select',
-        _options: ['block', 'inline-block', 'inline']
-    },
-    position: {
-        _type: 'select',
-        _options: ['relative', 'absolute', 'fixed']
-    },
-    float: {
-        _type: 'select',
-        _options: ['left', 'right']
-    },
-    'text-align': {
-        _type: 'select',
-        _options: ['center', 'left', 'right']
+                _value: v,
+                _options: ['relative', 'absolute', 'fixed']
+            };
+        case 'float':
+            return {
+                _type: 'select',
+                _value: v,
+                _options: ['left', 'right']
+            };
+        case 'text-align':
+            return {
+                _type: 'select',
+                _value: v,
+                _options: ['center', 'left', 'right']
+            };
+    }
+};
+
+const valueEncoder = (type, obj) => {
+    switch (type) {
+        case 'text':
+        case 'color':
+        case 'number':
+        case 'display':
+        case 'position':
+        case 'float':
+        case 'text-align':
+            return obj._value;
+        case 'size':
+            return obj.number._value + obj.unit._value;
+        case 'size_4':
+            return [valueEncoder('size', obj.top), valueEncoder('size', obj.right), valueEncoder('size', obj.bottom), valueEncoder('size', obj.left)].join(' ');
+        default:
+            return '';
     }
 };
 
@@ -84,11 +105,16 @@ const parse = (filePath, { converter } = {}) => {
     if (content == null) return null;
     let vars = {};
     let t = util.matchReg(content, /\$([a-zA-Z0-9$_-]+):\s*(.*?);\s*\/\/(.*)/g);
-    let varArr = t.matches.map(item => ({
-        name: item[1],
-        value: item[2],
-        desc: cloneObj(descPresets[item[3].trim()] || { _type: 'text' })
-    }));
+    let varArr = t.matches.map(item => {
+        let value = item[2].trim();
+        let type = (item[3] || 'text').trim();
+        return {
+            name: item[1],
+            value: value,
+            type: type,
+            desc: cloneObj(valueDecoder(type, value) || { _type: 'text', _value: value })
+        }
+    });
     return {
         vars: converter ? converter(varArr) : varArr,
         body: content.substr(t.offset)
@@ -99,7 +125,7 @@ const { StringDecoder } = require('string_decoder');
 const decoder = new StringDecoder('utf8');
 
 const stringify = (obj = {}) => {
-    let scss = (obj.vars || []).map(item => `$${item.name}: ${item.value}; // ${item.desc}`).join('\n') +
+    let scss = (obj.vars || []).map(item => `$${item.name}: ${item.value = valueEncoder(item.type, item.desc)}; // ${item.type}`).join('\n') +
         (obj.body || '');
     if (scss.trim() === '') return '';
     let output = decoder.write(sass.renderSync({
